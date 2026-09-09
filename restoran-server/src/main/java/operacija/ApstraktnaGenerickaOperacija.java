86d2e6a2ede07650b7ba1433fb5dee8d701e616d
Filip Oketic <filipoketic1@gmail.com>
2026-09-09 22:43:51 +0200

    Izmenjena ApstraktnaGenerickaOperacija - repozitorijum se prosledjuje kroz konstruktor radi testiranja

--- restoran-server/src/main/java/operacija/ApstraktnaGenerickaOperacija.java
+++ restoran-server/src/main/java/operacija/ApstraktnaGenerickaOperacija.java
@@ -1,25 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package operacija;
 
-import repository.Repository;
 import repository.db.DbRepository;
 import repository.db.impl.DbRepositoryGeneric;
 
 /**
+ * Apstraktna klasa koja predstavlja sablon za izvrsavanje svih sistemskih
+ * operacija u sistemu.
  *
- * @author Cofara
+ * Definise nepromenljiv redosled koraka: provera preduslova, zapocinjanje
+ * transakcije, izvrsavanje same operacije i potvrda transakcije. Ukoliko
+ * dodje do greske u bilo kom koraku, transakcija se ponistava.
+ *
+ * @author Filip Oketic
+ * @version 1.0
  */
 public abstract class ApstraktnaGenerickaOperacija {
-    
-    protected final Repository broker;
 
+    /**
+     * Repozitorijum preko kojeg operacija pristupa bazi podataka.
+     */
+    protected final DbRepository broker;
+
+    /**
+     * Kreira operaciju sa podrazumevanim repozitorijumom nad bazom podataka.
+     */
     public ApstraktnaGenerickaOperacija() {
-        this.broker = new DbRepositoryGeneric();
+        this(new DbRepositoryGeneric());
     }
-    
+
+    /**
+     * Kreira operaciju sa prosledjenim repozitorijumom.
+     * Ovaj konstruktor omogucava testiranje operacije bez stvarne baze podataka.
+     *
+     * @param broker repozitorijum koji operacija koristi za pristup podacima
+     */
+    public ApstraktnaGenerickaOperacija(DbRepository broker) {
+        this.broker = broker;
+    }
+
+    /**
+     * Izvrsava sistemsku operaciju po utvrdjenom sablonu.
+     *
+     * @param objekat objekat nad kojim se operacija izvrsava
+     * @param kljuc dodatni uslov ili parametar operacije, moze biti null
+     * @throws Exception ako preduslovi nisu ispunjeni ili ako operacija ne uspe
+     */
     public final void izvrsi(Object objekat, String kljuc) throws Exception {
         try {
             preduslovi(objekat);
@@ -29,30 +54,35 @@ public abstract class ApstraktnaGenerickaOperacija {
         } catch (Exception e) {
             ponistiTransakciju();
             throw e;
-        } finally {
-            //ugasiKonekciju();
         }
     }
 
+    /**
+     * Proverava da li su ispunjeni preduslovi za izvrsavanje operacije.
+     *
+     * @param param objekat nad kojim se operacija izvrsava
+     * @throws Exception ako preduslovi nisu ispunjeni
+     */
     protected abstract void preduslovi(Object param) throws Exception;
 
+    /**
+     * Izvrsava konkretnu sistemsku operaciju.
+     *
+     * @param param objekat nad kojim se operacija izvrsava
+     * @param kljuc dodatni uslov ili parametar operacije, moze biti null
+     * @throws Exception ako operacija ne uspe
+     */
     protected abstract void izvrsiOperaciju(Object param, String kljuc) throws Exception;
 
     private void zapocniTransakciju() throws Exception {
-        ((DbRepository) broker).connect();
+        broker.connect();
     }
 
     private void potvrdiTransakciju() throws Exception {
-        ((DbRepository) broker).commit();
+        broker.commit();
     }
 
     private void ponistiTransakciju() throws Exception {
-        ((DbRepository) broker).rollback();
+        broker.rollback();
     }
-
-    private void ugasiKonekciju() throws Exception {
-        ((DbRepository) broker).disconnect();
-    }
-
-    
 }
